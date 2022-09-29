5b14f97178d29d9aeaca7e0ef289d5e383d44557
marikuna246 <mgurabanidze92@gmail.com>
2022-09-29 06:44:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// let user1 = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+//   // 1.ამოიღეთ user ცვლადის სტუდენტის სტატუსი;
+
+//   console.log(user1["studentstatus"])
+
+//   //2.შექმენით მასივი, და დაბეჭდეთ სათითაოდ თითოეული ელემენტი(for da while ციკლით);
+
+//   animals = ["dog", "cat", "tiger", "lion"]
+//   for (let x of animals) {
+//     console.log(x)
+//   }
+
+//   let i=0;
+//   while (i <animals.lenght) {
+//     console.log(animals[i])
+//     i++;
+//    }
+ 
+//    //3.  შექმენით მასივი, და ამოიღეთ ყველა რიცხვი რომელიც მეტია 5ზე, for ციკლის გამოყენებით.
+
+//    let numbers = [66, 10, 2, 4, 11,1,17,45,3,55];
+//     for(let item of numbers)
+//     if (item>5){
+//     console.log(item);
+//    }
+
+//    // 4.
+//    let user = {
+//         name: "giorgi",
+//         age: 20,
+//         studentstatus: "active"
+//      };
+    
+//      if (user.age < 18 && user.studentstatus == "active") {
+//         console.log("hello");
+//      }else if(user.name == "levan") {
+//           console.log("hello levani");
+//         } else if (user.studentstatus == "active" || user.age <25) {
+//           console.log("hallo word");
+//         } else {
+//           console.log("error");
+//         }
+
+//    // 5 გამოიტანე მარტო სტრინგები.
+
+//    let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+//         for (let item of array) {
+//         if (typeof item =="string") {
+//         console.log(item);
+//       }
+//         } 
+ 
+//         // 6. გამოიტანეთ მხოლოდ დადებითი რიცხვები:yy 9oikk n
+
+
+//         let array1 = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10] ]
+//          for (let x of array1) {
+//           for (let positive of x) {
+//             if (positive >0)
+//             console.log(positive);
+//           }
+//          }
+
+//          // 7.
+
+//         //  let array3 = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+//         //  for (let item of array3) {
+//         //      if (item % 2 == 0)
+//         //      console.log(item);
+//         //  }
+
+//          let array4 = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+//          for (let item of array4) {
+//               if (item % 2 == 1) 
+//                 console.log(item);
+//               }
+         
+
+
+//          //8.
+//          let users = [
+//           {username: 'giorgi', status: false},
+//           {username: 'levani', status: false},
+//           {username: 'anna', status: true}
+//       ]
+
+//         for (let item of users) {
+//           if (item.status == true) {
+//           console.log(item);
+//          }
+//         }
+      
+
+
+
+
+       
+
+
+
+        
